c814953ef6af6f47ac3caedb08726295c3c3e3c2
Leigh Dodds <leigh@ldodds.com>
2019-05-04 15:35:11 +0100

    Updates

---
+++ precache-manifest.54df169ef2fa3a26faa1046fea7ebcd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f2c10250081cf1bd9a4d2250b80dba6",
+    "url": "/Open-Data-Viewer/static/media/markers-soft@2x.0f2c1025.png"
+  },
+  {
+    "revision": "79148a43587cbe5c43d24fa894a78ba3",
+    "url": "/Open-Data-Viewer/static/media/markers-soft.79148a43.png"
+  },
+  {
+    "revision": "ef0e5cf66d6c845314a5",
+    "url": "/Open-Data-Viewer/static/js/runtime~main.ef0e5cf6.js"
+  },
+  {
+    "revision": "dd145c1370fe7adbee0c",
+    "url": "/Open-Data-Viewer/static/js/main.dd145c13.chunk.js"
+  },
+  {
+    "revision": "d1771f53fe670591d224",
+    "url": "/Open-Data-Viewer/static/js/1.d1771f53.chunk.js"
+  },
+  {
+    "revision": "dd145c1370fe7adbee0c",
+    "url": "/Open-Data-Viewer/static/css/main.65b67257.chunk.css"
+  },
+  {
+    "revision": "d1771f53fe670591d224",
+    "url": "/Open-Data-Viewer/static/css/1.394ea3c6.chunk.css"
+  },
+  {
+    "revision": "632f0aadcca60a4fe678314c9edc8e7d",
+    "url": "/Open-Data-Viewer/index.html"
+  }
+];
